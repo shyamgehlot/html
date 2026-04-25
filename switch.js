dbc8302d3fc1ca809b18f551f5cd25c92714c979
shyamgehlot <gehlotdhannalal@gmail.com>
2026-04-25 11:21:51 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,23 @@
+let day = Number(prompt("enter a day"));
+
+switch (day) {
+  case 1:
+    console.log("monday");
+    break;
+
+  case 2:
+    console.log("tuesday");
+    break;
+  case 3:
+    console.log("wednesday");
+    break;
+  case 4:
+    console.log("thursday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  default:
+    console.log("sunday");
+    break;
+}
